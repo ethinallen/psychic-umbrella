2727d6b6eb34ecca89581f9ca860a9e3153dca42
Drew <abemery@clemson.edu>
2021-09-21 14:07:46 -0700

    change naming; can't add alert to seattle page yet

--- src/views/Seattle/Seattle.js
+++ src/views/Seattle/Seattle.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import MapContainer from './MapContainer'
+
+
 import './Seattle.css'
 
 
 
 class Seattle extends Component {
+
+
   render() {
+
     return (
+      <>
       <MapContainer />
+
+      </>
     );
   }
 }
