11ed263d473724db617474d5596de4041f045c4a
Drew <abemery@clemson.edu>
2021-12-03 17:20:28 -0800

    looks a little less like trash

--- src/views/Home/Page.js
+++ src/views/Home/Page.js
@@ -6,21 +6,21 @@ function Page(props) {
 
   if (props.backg) {
     return (
-      <>
-        <div className="topHalf">
+      <div className="imagecontainer">
+        <div className="leftHalf">
         <div className="media-container">
-          <img className="photo" alt="" src={props.backg}/>
+          <img className="photo" alt="alt" src={props.backg}/>
         </div>
+        </div>
+        <div className="rightHalf">
         <div className="header">
           {props.content}
         </div>
-        </div>
-        <div className="content-container">
           <div className="sub">
             {props.sub}
           </div>
         </div>
-      </>
+      </div>
 
     );
   }
