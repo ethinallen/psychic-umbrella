0e786534fc11917d8b8635d208dd22a3d8c2cbd9
Drew <abemery@clemson.edu>
2022-03-03 20:29:00 -0500

    change to custom domain

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // components
 import Layout from "./Layout";
 
 // pages
 import Error from "../pages/error";
-import Login from "../pages/login";
 
 export default function App() {
   // global
