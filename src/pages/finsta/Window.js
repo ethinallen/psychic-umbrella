55c2bc14ef6d0b434905b61afdb1e6ec0a0e9f8c
Drew <abemery@clemson.edu>
2022-03-05 10:56:31 -0800

    variable container width

--- src/pages/finsta/Window.js
+++ src/pages/finsta/Window.js
@@ -6,12 +6,35 @@ import PageTitle from '../../components/PageTitle';
 import useStyles from "./styles";
 
 export default function Window(props) {
+  var containerWidth = makeWidth();
 
+  console.log(containerWidth);
   var classes = useStyles();
 
     return (
-      <Grid container item xs={4} alignItems="center" justifyContent="center">
+      <Grid container item xs={containerWidth} alignItems="center" justifyContent="center">
         <img className={classes.window} alt="" src={props.source} ></img>
       </Grid>
     );
+}
+
+
+function makeWidth() {
+  var width = window.screen.availWidth;
+  console.log(width);
+  switch (true) {
+    case (width < 500):
+      console.log("yeet");
+      return 6;
+      break;
+    case (width < 2000):
+      return 4;
+      break;
+    case (width > 2000):
+      return 3;
+      break;
+    default:
+      return 3;
+      break;
+  }
 }
