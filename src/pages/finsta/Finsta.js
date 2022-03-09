25de91bf6e485d8508ef327d23d34965addeed27
Drew <abemery@clemson.edu>
2022-03-09 13:54:34 -0800

    shuffle

--- src/pages/finsta/Finsta.js
+++ src/pages/finsta/Finsta.js
@@ -9,8 +9,26 @@ import { graphql } from "react-apollo";
 
 const getUserProfile = async () => {
     const response = await API.graphql(graphqlOperation(GetUser));
-    return response.data.getProfile.addrs;
+    return shuffle(response.data.getProfile.addrs);
 };
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
 
 
 function Finsta() {
